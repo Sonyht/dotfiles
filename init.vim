9fb2b3e2f7201f1bef59cebee8bc1d85a74932c0
Knuetel <Ken.Knuetel@bain.com>
2022-09-19 14:42:07 +0200

    make treesitter work

--- init.vim
+++ init.vim
@@ -3,6 +3,7 @@ Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.0' }
 Plug 'gruvbox-community/gruvbox'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'nvim-treesitter/nvim-treesitter-context'
 
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/nvim-cmp'
@@ -10,6 +11,8 @@ Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'L3MON4D3/LuaSnip'
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'onsails/lspkind-nvim'
+
+"Plug 'luk400/vim-jukit'  
 "Plug 'projekt0n/github-nvim-theme'
 call plug#end()
 colorscheme gruvbox
@@ -18,6 +21,8 @@ colorscheme gruvbox
 lua require('Sonyht/options')
 lua require('Sonyht/lsp')
 lua require('Sonyht/keymap')
+lua require('Sonyht/treesitter')
+"lua require('Sonyht/jukit')
 
 
 nnoremap <leader>ff :Telescope find_files<cr>
--- main.py
+++ main.py
@@ -6,4 +6,43 @@ df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_Su
 print(df)
 x = ['test']
 np.array(x)
+def function(x,y):
+    """ long text
+    long text
 
+    from LCA.models.mixed import MixedLCA
+    from LCA.models.mixed import MixedLCA
+    from LCA.models.mixed import MixedLCA
+    from LCA.models.mixed import MixedLCA
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_SurveyData.sav")
+    print(df)
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_SurveyData.sav")
+    print(df)
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    from LCA.models.mixed import MixedLCA
+    from LCA.models.mixed import MixedLCA
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_SurveyData.sav")
+    print(df)
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_SurveyData.sav")
+    print(df)
+    from datetime import datetime
+    import numpy as np
+    import pandas as pd
+    df = pd.read_spss(r"C:\Users\64169\Downloads\8. ConsumerProducts_CoffeeMakers_SurveyData.sav")
+    print(df)
+
+    """ 
